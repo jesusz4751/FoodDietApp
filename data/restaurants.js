f4f4133f133a848fa18b5cd42153b2bcf1644b67
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-01-07 09:50:44 -0800

    McDonald's data

---
+++ data/restaurants.js
@@ -0,0 +1,169 @@
+const restaurants = {
+  mcDonalds: {
+    entree: {
+      bigMac: {
+        name: 'Big Mac',
+        image: '',
+        calories: 580,
+        protein: 25,
+        carbs: 45,
+        fat: 34
+      },
+      qpCheese: {
+        name: 'Quarter Pounder with Cheese',
+        image: '',
+        calories: 520,
+        protein: 30,
+        carbs: 42,
+        fat: 26
+      },
+      doubleQpCheese: {
+        name: 'Double Quarter Pounder with Cheese',
+        image: '',
+        calories: 740,
+        protein: 48,
+        carbs: 43,
+        fat: 42
+      },
+      mcDouble: {
+        name: 'McDouble',
+        image: '',
+        calories: 390,
+        protein: 22,
+        carbs: 32,
+        fat: 20
+      },
+      eggMcMuffin: {
+        name: 'Egg McMuffin',
+        image: '',
+        calories: 310,
+        protein: 17,
+        carbs: 30,
+        fat: 13
+      },
+      mcCrispy: {
+        name: 'McCrispy',
+        image: '',
+        calories: 470,
+        protein: 26,
+        carbs: 46,
+        fat: 20
+      },
+      deluxeMcCrispy: {
+        name: 'Deluxe McCrispy',
+        image: '',
+        calories: 530,
+        protein: 27,
+        carbs: 48,
+        fat: 26
+      },
+      spicyMcCrispy: {
+        name: 'Spicy McCrispy',
+        image: '',
+        calories: 530,
+        protein: 27,
+        carbs: 48,
+        fat: 26,
+      },
+      spicyDeluxeMcCrispy: {
+        name: 'Spicy Deluxe McCrispy',
+        image: '',
+        calories: 530,
+        protein: 27,
+        carbs: 49,
+        fat: 26
+      },
+      mcChicken: {
+        name: 'McChicken',
+        image: '',
+        calories: 390,
+        protein: 14,
+        carbs: 38,
+        fat: 21
+      },
+      fish: {
+        name: 'Filet-O-Fish',
+        image: '',
+        calories: 380,
+        protein: 16,
+        carbs: 38,
+        fat: 19
+      }
+    },
+    side: {
+      fries: {
+        name: 'World Famous Fries',
+        image: '',
+        calories: 320,
+        protein: 5,
+        carbs: 43,
+        fat: 15
+      },
+      mcNuggets: {
+        name: '6 piece McNuggets',
+        image: '',
+        type: 'side',
+        calories: 250,
+        protein: 14,
+        carbs: 15,
+        fat: 15
+      },
+      apple: {
+        name: 'Apple Slices',
+        image: '',
+        calories: 15,
+        protein: 0,
+        carbs: 4,
+        fat: 0
+      },
+      oreoMcFlurry: {
+        name: 'McFlurry with OREO Cookies',
+        image: '',
+        calories: 410,
+        protein: 10,
+        carbs: 64,
+        fat: 13
+      },
+      cone: {
+        name: 'Vanilla Cone',
+        image: '',
+        calories: 200,
+        protein: 5,
+        carbs: 33,
+        fat: 5
+      },
+      vanillaShake: {
+        name: 'Vanilla Shake',
+        image: '',
+        calories: 480,
+        protein: 10,
+        carbs: 80,
+        fat: 13
+      },
+      sundae: {
+        name: 'Hot Fudge Sundae',
+        image: '',
+        calories: 330,
+        protein: 8,
+        carbs: 51,
+        fat: 10
+      },
+      pie: {
+        name: 'Baked Apple Pie',
+        image: '',
+        calories: 230,
+        protein: 2,
+        carbs: 33,
+        fat: 11
+      },
+      cookie: {
+        name: 'Chocolate Chip Cookie',
+        image: '',
+        calories: 170,
+        protein: 2,
+        carbs: 22,
+        fat: 8
+      }
+    }
+  }
+}
