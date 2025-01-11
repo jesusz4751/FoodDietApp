3779eceb688dd72283e9562b6324bcb61db04761
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-01-11 11:12:21 -0800

    Replaced images with amount indicator. Moved food item to separate css file

--- script/entree.js
+++ script/entree.js
@@ -1,34 +1,42 @@
 import {restaurants} from "../data/restaurants.js";
 
-const userCalories = sessionStorage.getItem('calories');
+// const userCalories = sessionStorage.getItem('calories');
+const userCalories = 1000;
+// if (!userCalories){
+//   window.location.href = 'home.html';
+// }
 let entrees = restaurants.mcDonalds.entree;
 let allItemsHTML = '';
 
 Object.values(entrees).forEach((item) => {
   if (item.calories <= userCalories){
     allItemsHTML += `<div class="food-item">
-    <div class="food-information-container">
-      <p class="food-name">${item.name}</p>
-      <div class="macro-container">
-        <p class="macro">Calories</p>
-        <p class="macro-amount">${item.calories}</p>
+      <div class="food-information-container">
+        <p class="food-name">${item.name}</p>
+        <div class="macro-container">
+          <p class="macro">Calories</p>
+          <p class="macro-amount">${item.calories}</p>
+        </div>
+        <div class="macro-container">
+          <p class="macro">Protein</p>
+          <p class="macro-amount">${item.protein}g</p>
+        </div>
+        <div class="macro-container">
+          <p class="macro">Carbs</p>
+          <p class="macro-amount">${item.carbs}g</p>
+        </div>
+        <div class="macro-container">
+          <p class="macro">Fat</p>
+          <p class="macro-amount">${item.fat}g</p>
+        </div>
       </div>
-      <div class="macro-container">
-        <p class="macro">Protein</p>
-        <p class="macro-amount">${item.protein}g</p>
+      <div class="quantity-selector">
+        <button class="minus amount-button fadeReverse"><i class="fa-solid fa-minus"></i></button>
+        <p class="amount-indicator">1</p>
+        <button class="plus amount-button fadeReverse"><i class="fa-solid fa-plus"></i></button>
       </div>
-      <div class="macro-container">
-        <p class="macro">Carbs</p>
-        <p class="macro-amount">${item.carbs}g</p>
-      </div>
-      <div class="macro-container">
-        <p class="macro">Fat</p>
-        <p class="macro-amount">${item.fat}g</p>
-      </div>
-    </div>
-    <img src="images/McDonalds/mccrispy-removebg-preview.png" class="food-image">
-    <button class="food-select-button fade" id="button1">Select</button>
-  </div>`;
+      <button class="food-select-button fade">Select</button>
+    </div>`;
   }
 });
 document.querySelector('.food-item-container').innerHTML = allItemsHTML;
--- script/home.js
+++ script/home.js
@@ -2,5 +2,5 @@ const nextButton = document.getElementById('next-button');
 nextButton.addEventListener('click', () => {
   const calories = document.getElementById('calorie-input').value;
   sessionStorage.setItem('calories', calories);
-  window.location.href = 'food_selector.html';
+  window.location.href = 'entree.html';
 });
