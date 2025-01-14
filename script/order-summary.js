b954edfdd6674597310bb7a34415ad8f90ecf9e5
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-01-14 12:04:04 -0800

    Update order summary page using js, restored main page image

--- script/order-summary.js
+++ script/order-summary.js
@@ -6,7 +6,7 @@ const userCalories = 1000;
 const entree = sessionStorage.getItem('entree');
 const entreeAmount = sessionStorage.getItem('entreeAmount');
 const side = sessionStorage.getItem('side');
-const sideAmout = sessionStorage.getItem('sideAmount');
+const sideAmount = sessionStorage.getItem('sideAmount');
 const totalCalories = (restaurants.mcDonalds.entree[entree].calories * entreeAmount) + (restaurants.mcDonalds.side[side].calories * entreeAmount);
 const totalProtein = (restaurants.mcDonalds.entree[entree].protein * entreeAmount) + (restaurants.mcDonalds.side[side].protein * entreeAmount);
 const totalCarbs = (restaurants.mcDonalds.entree[entree].carbs * entreeAmount) + (restaurants.mcDonalds.side[side].carbs * entreeAmount);
@@ -15,3 +15,8 @@ document.getElementById('calories').innerText = totalCalories;
 document.getElementById('protein').innerText = totalProtein + 'g';
 document.getElementById('carbs').innerText = totalCarbs + 'g';
 document.getElementById('fat').innerText = totalFat + 'g';
+
+document.getElementById('entree-name').innerText = restaurants.mcDonalds.entree[entree].name;
+document.getElementById('entree-amount').innerText = entreeAmount + 'x';
+document.getElementById('side-name').innerText = restaurants.mcDonalds.side[side].name;
+document.getElementById('side-amount').innerText = sideAmount + 'X';
