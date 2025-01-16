d8fad802535f99179d80aec3b33b5997eda2215c
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-01-16 13:22:05 -0800

    Fixed bug for no sides instance on order summary page

--- script/order-summary.js
+++ script/order-summary.js
@@ -1,16 +1,30 @@
 import {restaurants} from "../data/restaurants.js";
 
-// const userCalories = sessionStorage.getItem('calories');
-const userCalories = 1000;
-
 const entree = sessionStorage.getItem('entree');
 const entreeAmount = sessionStorage.getItem('entreeAmount');
-const side = sessionStorage.getItem('side');
-const sideAmount = sessionStorage.getItem('sideAmount');
-const totalCalories = (restaurants.mcDonalds.entree[entree].calories * entreeAmount) + (restaurants.mcDonalds.side[side].calories * sideAmount);
-const totalProtein = (restaurants.mcDonalds.entree[entree].protein * entreeAmount) + (restaurants.mcDonalds.side[side].protein * sideAmount);
-const totalCarbs = (restaurants.mcDonalds.entree[entree].carbs * entreeAmount) + (restaurants.mcDonalds.side[side].carbs * sideAmount);
-const totalFat = (restaurants.mcDonalds.entree[entree].fat * entreeAmount) + (restaurants.mcDonalds.side[side].fat * sideAmount);
+let side = '';
+let sideAmount = 1;
+let totalCalories = restaurants.mcDonalds.entree[entree].calories * entreeAmount;
+let totalProtein = restaurants.mcDonalds.entree[entree].protein * entreeAmount;
+let totalCarbs = restaurants.mcDonalds.entree[entree].carbs * entreeAmount;
+let totalFat = restaurants.mcDonalds.entree[entree].fat * entreeAmount; 
+
+//If there are side items, adds them to hte page
+if (!sessionStorage.getItem('skip')){
+  side = sessionStorage.getItem('side');
+  sideAmount = sessionStorage.getItem('sideAmount');
+  document.querySelector('.food-section').innerHTML += `
+  <div class="food-item">
+    <p class="food-amount" id="side-amount"></p>
+    <p class="food-title" id="side-name"></p>
+  </div>`
+  document.getElementById('side-name').innerText = restaurants.mcDonalds.side[side].name;
+  document.getElementById('side-amount').innerText = sideAmount + 'x';
+  totalCalories += restaurants.mcDonalds.side[side].calories * sideAmount;
+  totalProtein += restaurants.mcDonalds.side[side].protein * sideAmount;
+  totalCarbs += restaurants.mcDonalds.side[side].carbs * sideAmount;
+  totalFat += restaurants.mcDonalds.side[side].fat * sideAmount;
+}
 document.getElementById('calories').innerText = totalCalories;
 document.getElementById('protein').innerText = totalProtein + 'g';
 document.getElementById('carbs').innerText = totalCarbs + 'g';
@@ -18,5 +32,3 @@ document.getElementById('fat').innerText = totalFat + 'g';
 
 document.getElementById('entree-name').innerText = restaurants.mcDonalds.entree[entree].name;
 document.getElementById('entree-amount').innerText = entreeAmount + 'x';
-document.getElementById('side-name').innerText = restaurants.mcDonalds.side[side].name;
-document.getElementById('side-amount').innerText = sideAmount + 'x';
