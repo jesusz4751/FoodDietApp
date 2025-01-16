82d9e47d4cfee6c630019894da3ca247f7416c9d
jesusz4751 <jesuseduardoz.4751@gmail.com>
2025-01-16 14:16:00 -0800

    Added data for other restaurants

--- data/restaurants.js
+++ data/restaurants.js
@@ -3,88 +3,77 @@ export const restaurants = {
     entree: {
       minCalories: 310,
       bigMac: {
-        name: 'Big Mac',
-        image: '',
+        name: 'Big Mac®',
         calories: 580,
         protein: 25,
         carbs: 45,
         fat: 34
       },
       qpCheese: {
-        name: 'Quarter Pounder with Cheese',
-        image: '',
+        name: 'Quarter Pounder with Cheese®',
         calories: 520,
         protein: 30,
         carbs: 42,
         fat: 26
       },
       doubleQpCheese: {
-        name: 'Double Quarter Pounder with Cheese',
-        image: '',
+        name: 'Double Quarter Pounder with Cheese®',
         calories: 740,
         protein: 48,
         carbs: 43,
         fat: 42
       },
       mcDouble: {
-        name: 'McDouble',
-        image: '',
+        name: 'McDouble®',
         calories: 390,
         protein: 22,
         carbs: 32,
         fat: 20
       },
       eggMcMuffin: {
-        name: 'Egg McMuffin',
-        image: '',
+        name: 'Egg McMuffin®',
         calories: 310,
         protein: 17,
         carbs: 30,
         fat: 13
       },
       mcCrispy: {
-        name: 'McCrispy',
-        image: '',
+        name: 'McCrispy®',
         calories: 470,
         protein: 26,
         carbs: 46,
         fat: 20
       },
       deluxeMcCrispy: {
-        name: 'Deluxe McCrispy',
-        image: '',
+        name: 'Deluxe McCrispy®',
         calories: 530,
         protein: 27,
         carbs: 48,
         fat: 26
       },
       spicyMcCrispy: {
-        name: 'Spicy McCrispy',
-        image: '',
+        name: 'Spicy McCrispy®',
         calories: 530,
         protein: 27,
         carbs: 48,
         fat: 26,
       },
       spicyDeluxeMcCrispy: {
-        name: 'Spicy Deluxe McCrispy',
-        image: '',
+        name: 'Spicy Deluxe McCrispy®',
         calories: 530,
         protein: 27,
         carbs: 49,
         fat: 26
       },
       mcChicken: {
-        name: 'McChicken',
-        image: '',
+        name: 'McChicken®',
         calories: 390,
         protein: 14,
         carbs: 38,
         fat: 21
       },
       fish: {
-        name: 'Filet-O-Fish',
-        image: '',
+        name: 'Filet-O-Fish®',
         calories: 380,
         protein: 16,
         carbs: 38,
@@ -94,16 +83,14 @@ export const restaurants = {
     side: {
       minCalories: 15,
       fries: {
-        name: 'World Famous Fries',
-        image: '',
+        name: 'World Famous Fries®',
         calories: 320,
         protein: 5,
         carbs: 43,
         fat: 15
       },
       mcNuggets: {
-        name: '6 piece McNuggets',
-        image: '',
+        name: '6 piece McNuggets®',
         type: 'side',
         calories: 250,
         protein: 14,
@@ -112,15 +99,13 @@ export const restaurants = {
       },
       apple: {
         name: 'Apple Slices',
-        image: '',
         calories: 15,
         protein: 0,
         carbs: 4,
         fat: 0
       },
       oreoMcFlurry: {
-        name: 'McFlurry with OREO Cookies',
-        image: '',
+        name: 'McFlurry® with OREO® Cookies',
         calories: 410,
         protein: 10,
         carbs: 64,
@@ -128,7 +113,6 @@ export const restaurants = {
       },
       cone: {
         name: 'Vanilla Cone',
-        image: '',
         calories: 200,
         protein: 5,
         carbs: 33,
@@ -136,7 +120,6 @@ export const restaurants = {
       },
       vanillaShake: {
         name: 'Vanilla Shake',
-        image: '',
         calories: 480,
         protein: 10,
         carbs: 80,
@@ -144,7 +127,6 @@ export const restaurants = {
       },
       sundae: {
         name: 'Hot Fudge Sundae',
-        image: '',
         calories: 330,
         protein: 8,
         carbs: 51,
@@ -152,7 +134,6 @@ export const restaurants = {
       },
       pie: {
         name: 'Baked Apple Pie',
-        image: '',
         calories: 230,
         protein: 2,
         carbs: 33,
@@ -160,11 +141,211 @@ export const restaurants = {
       },
       cookie: {
         name: 'Chocolate Chip Cookie',
-        image: '',
         calories: 170,
         protein: 2,
         carbs: 22,
         fat: 8
+      }
+    }
+  },
+  jackInTheBox: {
+    entree: {
+      jumboJack: {
+        name: 'Jumbo Jack®',
+        calories: 520,
+        protein: 23,
+        carbs: 32,
+        fat: 33
+      },
+      doubleJack: {
+        name: 'Double Jack®',
+        calories: 830,
+        protein: 46,
+        carbs: 33,
+        fat: 58
+      },
+      baconUltimate: {
+        name: 'Bacon Ultimate Cheeseburger®',
+        calories: 930,
+        protein: 55,
+        carbs: 32,
+        fat: 65
+      },
+      spicyChicken: {
+        name: "Jack's Spicy Chicken Sandwich®",
+        calories: 690,
+        protein: 27,
+        carbs: 51,
+        fat: 42
+      },
+      spicyChickenCheese: {
+        name: "Jack's Spicy Chicken Sandwich® with Cheese",
+        calories: 720,
+        protein: 29,
+        carbs: 51,
+        fat: 45
+      },
+      homestyleRanch: {
+        name: 'Homestyle Ranch Chicken Club®',
+        calories: 790,
+        protein: 35,
+        carbs: 69,
+        fat: 42
+      },
+      chickenWrap: {
+        name: 'Grilled Chicken Fajita Jack Wrap®',
+        calories: 290,
+        protein: 18,
+        carbs: 26,
+        fat: 13
+      }
+    },
+    side: {
+      fries: {
+        name: 'French Fries (Medium)',
+        calories: 430,
+        protein: 5,
+        carbs: 58,
+        fat: 20
+      },
+      curFries: {
+        name: 'Curly Fries (Medium)',
+        calories: 430,
+        protein: 5,
+        carbs: 46,
+        fat: 25
+      },
+      oniRings: {
+        name: 'Onion Rings',
+        calories: 440,
+        protein: 6,
+        carbs: 52,
+        fat: 24
+      },
+      nuggets: {
+        name: '8 piece Chicken Nuggets',
+        calories: 380,
+        protein: 15,
+        carbs: 20,
+        fat: 27
+      },
+      tacos: {
+        name: 'Taco',
+        calories: 170,
+        protein: 6,
+        carbs: 16,
+        fat: 9
+      },
+      tiny: {
+        name: 'Tiny Tacos® (13 pc)',
+        calories: 460,
+        protein: 14,
+        carbs: 54,
+        fat: 21
+      }
+    }
+  },
+  tacoBell: {
+    entree: {
+      softTaco: {
+        name: 'Soft Taco',
+        calories: 180,
+        protein: 9,
+        carbs: 18,
+        fat: 8
+      },
+      crunchyTaco: {
+        name: 'Crunchy Taco',
+        calories: 170,
+        protein: 8,
+        carbs: 13,
+        fat: 10
+      },
+      durTaco: {
+        name: 'Nacho Cheese Doritos® Locos Taco',
+        calories: 170,
+        protein: 8,
+        carbs: 12,
+        fat: 10
+      },
+      durTacoSup: {
+        name: 'Nacho Cheese Doritos® Locos Taco Supreme®',
+        calories: 190,
+        protein: 8,
+        carbs: 14,
+        fat: 11
+      },
+      cheeGorCru: {
+        name: 'Cheesy Gordita Crunch',
+        calories: 490,
+        protein: 20,
+        carbs: 41,
+        fat: 28
+      },
+      chaSup: {
+        name: 'Chalupa Supreme®',
+        calories: 360,
+        protein: 12,
+        carbs: 31,
+        fat: 20
+      },
+      chiQue: {
+        name: 'Cantina Chicken Quesadilla®',
+        calories: 570,
+        protein: 29,
+        carbs: 43,
+        fat: 32
+      },
+      fiveBur: {
+        name: 'Beefy 5-Layer Burrito',
+        calories: 490,
+        protein: 17,
+        carbs: 64,
+        fat: 18
+      },
+      cruSup: {
+        name: 'Crunchwrap Supreme®',
+        calories: 540,
+        protein: 15,
+        carbs: 73,
+        fat: 20
+      }
+    },
+    side: {
+      nacFri: {
+        name: 'Nacho Fries',
+        calories: 330,
+        protein: 4,
+        carbs: 36,
+        fat: 19
+      },
+      chips: {
+        name: 'Chips and Nacho Cheese Sauce',
+        calories: 220,
+        protein: 3,
+        carbs: 24,
+        fat: 13
+      },
+      cinTwi: {
+        name: 'Cinnanom Twists',
+        calories: 170,
+        protein: 1,
+        carbs: 27,
+        fat: 6
+      },
+      cheFie: {
+        name: 'Cheesy Fiesta Potatoes',
+        calories: 240,
+        protein: 3,
+        carbs: 28,
+        fat: 13
+      },
+      blaRi: {
+        name: 'Black Beans and Rice',
+        calories: 160,
+        protein: 4,
+        carbs: 25,
+        fat: 4.5
       }
     }
   }
